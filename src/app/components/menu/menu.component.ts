d44b198b8aadcc534f775b9d51e102181c4cd424
RojoUnix <rojounix26@gmail.com>
2019-10-29 05:54:45 -0600

    Se modifica archivo para opciones en el Menú

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -8,24 +8,13 @@ import { ROLES } from '../../config/config';
 	styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
-    usuario: number;
-	opciones: any = [
-		{
-			nombre: 'Asistencia',
-			icono: 'hammer',
-			link: ['/alumno/asistencia']
-		},
-		{
-			nombre: 'Ticket',
-			icono: 'hammer',
-			link: ['/alumno/tickets']
-		},
-		{
-			nombre: 'Solicitud de laboratorios',
-			icono: 'hammer',
-			link: ['/alumno/solicitud']
-		}
-	]
+	usuario: number;
+
+	esAlumno: boolean;
+	esAdmin: boolean;
+	esProfesor: boolean;
+
+	opciones: any;
 	constructor( public authService: AuthService ) { }
 
 	ngOnInit() {
@@ -33,15 +22,58 @@ export class MenuComponent implements OnInit {
 		if(this.authService.rol == ROLES.ALUMNO){
 			console.log('Entro Alumno');
 			this.esAlumno= true;
+			this.opciones = [
+				{
+					nombre: 'Asistencia',
+					icono: 'hammer',
+					link: ['/alumno/asistencia']
+				},
+				{
+					nombre: 'Ticket',
+					icono: 'hammer',
+					link: ['/alumno/tickets']
+				},
+				{
+					nombre: 'Solicitud de laboratorios',
+					icono: 'hammer',
+					link: ['/alumno/solicitud']
+				}
+			];
 			
 		} else if(this.authService.rol == ROLES.PROFESOR){
-			//Consultando tickets para Alumno o Profesor
 			console.log('Entro Profesor');
 			this.esProfesor = true;
-		}else{
-			// Consultando tickets para Administrador (Todos los tickets)
+			this.opciones = [
+				{
+					nombre: 'Asistencia',
+					icono: 'hammer',
+					link: ['/alumno/asistencia']
+				},
+				{
+					nombre: 'Ticket',
+					icono: 'hammer',
+					link: ['/alumno/tickets']
+				},
+				{
+					nombre: 'Solicitud de laboratorios',
+					icono: 'hammer',
+					link: ['/alumno/solicitud']
+				}
+			];
+		}else if(this.authService.rol == ROLES.ADMINISTRADOR){
 			this.esAdmin = true;
-			this.getTicketsAdmin();
+			this.opciones = [
+				{
+					nombre: 'Asistencia',
+					icono: 'hammer',
+					link: ['/alumno/asistencia']
+				},
+				{
+					nombre: 'Ticket',
+					icono: 'hammer',
+					link: ['/alumno/tickets']
+				}
+			];
 		}
 	}
 	
