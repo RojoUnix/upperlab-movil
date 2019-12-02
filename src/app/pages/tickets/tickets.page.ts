69fe83dbceac532dc417f0ea41c539ce87062998
GeroCoded <luisfergeronimo@gmail.com>
2019-12-02 08:43:15 -0600

    Alert de ayuda en Tickets para los colores

--- src/app/pages/tickets/tickets.page.ts
+++ src/app/pages/tickets/tickets.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FcmService } from '../../services/fcm.service';
-import { ToastController } from '@ionic/angular';
+import { ToastController, AlertController } from '@ionic/angular';
 import { tap } from 'rxjs/operators';
 
 @Component({
@@ -10,7 +10,7 @@ import { tap } from 'rxjs/operators';
 })
 export class TicketsPage implements OnInit {
 	
-	constructor( private fcm: FcmService, private toastCtrl: ToastController ) {}
+	constructor( private fcm: FcmService, private toastCtrl: ToastController, private alertCtrl: AlertController ) {}
 	
 	ngOnInit() {
 		this.fcm.getToken();
@@ -27,6 +27,28 @@ export class TicketsPage implements OnInit {
 			duration: 2000
 		});
 		toast.present();
+	}
+
+	
+
+	async mostrarAlertAyuda() {
+		const alert = await this.alertCtrl.create({
+			header: '¿Qué significa el color?',
+			subHeader: 'El color debajo del ticket representa el estado en el que se encuentra.',
+			message: '<strong>AZUL</strong>: '
+			+ 'Tu ticket acaba de ser levantado y aún no es atendido.<br><br>'
+			+ '<strong>NARANJA</strong>: '
+			+ 'Tu ticket está en proceso de atención.<br><br>'
+			+ '<strong>VERDE</strong>: '
+			+ 'Tu ticket fue resuelto.<br><br>'
+			+ '<strong>GRIS</strong>: '
+			+ 'Tu ticket fue cancelado.<br><br>'
+			+ '<strong>ROJO</strong>: '
+			+ 'Tu ticket no pudo ser resuelto.',
+			buttons: ['ENTENDIDO']
+		});
+		
+		await alert.present();
 	}
 
 }
--- src/app/services/alert.service.ts
+++ src/app/services/alert.service.ts
@@ -10,16 +10,16 @@ export class AlertService {
 	constructor(private alertController: AlertController) { }
 	
 	
-
+	
 	/**
-	* Muestra un pop-up con la animación de carga para hacerle saber al usuario
-	* que el contenido está cargando y evitar que haga algo más. Para cerrar
-	* esta animación debe llamarse al método `cerrar()`.
-	* 
-	* @param mensaje Mensaje OPCIONAL que se muestra al usuario mientras carga.
-	* Si no se provee un mensjae se asignará por defecto a 'Cargando...'
-	* @return void
-	*/
+	 * Muestra un pop-up con la animación de carga para hacerle saber al usuario
+	 * que el contenido está cargando y evitar que haga algo más. Para cerrar
+	 * esta animación debe llamarse al método `cerrar()`.
+	 * 
+	 * @param mensaje Mensaje OPCIONAL que se muestra al usuario mientras carga.
+	 * Si no se provee un mensjae se asignará por defecto a 'Cargando...'
+	 * @return void
+	 */
 	
 	async cargando(header: string = 'Cargando...', subHeader: string) {
 		const alert = await this.alertController.create({
@@ -31,44 +31,44 @@ export class AlertService {
 	}
 	
 	/**
-	* Cierra el SweetAlert actual.
-	*/
+	 * Cierra el SweetAlert actual.
+	 */
 	cerrar() {
 		// Swal.close();
 	}
 	
 	/**
-	* Muestra un SweetAlert de confirmación de eliminación para preguntarle al
-	* usuario si está seguro de eliminar X información.
-	* 
-	* @param nombreElemento String para mostrarle al usuario qué elemento va
-	* a eliminar. Esto se muestra de la siguiente forma:  
-	* `'Eliminará permanentemente a {nombreElemento}'`
-	*/
+	 * Muestra un SweetAlert de confirmación de eliminación para preguntarle al
+	 * usuario si está seguro de eliminar X información.
+	 * 
+	 * @param nombreElemento String para mostrarle al usuario qué elemento va
+	 * a eliminar. Esto se muestra de la siguiente forma:  
+	 * `'Eliminará permanentemente a {nombreElemento}'`
+	 */
 	// confirmarEliminacion(nombreElemento: string): Promise<Boolean> {
-		// return Swal.fire({
-		// 	title: '¿Está seguro de eliminar?',
-		// 	html: `Eliminará permanentemente a <strong></strong>`,
-		// 	type: 'warning',
-		// 	showCancelButton: true,
-		// 	cancelButtonColor: '#5A6268', // '#3085d6',
-		// 	confirmButtonColor: '#d33',
-		// 	cancelButtonText: 'No',
-		// 	confirmButtonText: 'Sí',
-		// 	reverseButtons: true,
-		// 	focusCancel: true,
-		// 	onBeforeOpen: () => {
-		// 		Swal.getContent().querySelector('strong').textContent = nombreElemento;
-		// 	}
-		// });
+	// return Swal.fire({
+	// 	title: '¿Está seguro de eliminar?',
+	// 	html: `Eliminará permanentemente a <strong></strong>`,
+	// 	type: 'warning',
+	// 	showCancelButton: true,
+	// 	cancelButtonColor: '#5A6268', // '#3085d6',
+	// 	confirmButtonColor: '#d33',
+	// 	cancelButtonText: 'No',
+	// 	confirmButtonText: 'Sí',
+	// 	reverseButtons: true,
+	// 	focusCancel: true,
+	// 	onBeforeOpen: () => {
+	// 		Swal.getContent().querySelector('strong').textContent = nombreElemento;
+	// 	}
+	// });
 	// }
 	
 	/**
-	* Muestra un SweetAlert de confirmación para preguntarle al usuario si
-	* está seguro de realizar alguna acción.
-	* 
-	* @param text Texto que se muestra como subtítulo del SweetAlert.
-	*/
+	 * Muestra un SweetAlert de confirmación para preguntarle al usuario si
+	 * está seguro de realizar alguna acción.
+	 * 
+	 * @param text Texto que se muestra como subtítulo del SweetAlert.
+	 */
 	async confirmarAccion(header: string, subHeader: string) {
 		const alert = await this.alertController.create({
 			header,
@@ -106,12 +106,12 @@ export class AlertService {
 	// }
 	
 	/**
-	* Muestra un SweetAlert de tipo `error`.
-	* 
-	* @param title Título del SweetAlert
-	* @param text Texto del SweetAlert
-	* @return Promise<SweetAlertResult>
-	*/
+	 * Muestra un SweetAlert de tipo `error`.
+	 * 
+	 * @param title Título del SweetAlert
+	 * @param text Texto del SweetAlert
+	 * @return Promise<SweetAlertResult>
+	 */
 	// error(title: string = 'Error', text: string): Promise<SweetAlertResult> {
 	// 	return Swal.fire({
 	// 		title,
@@ -119,7 +119,7 @@ export class AlertService {
 	// 		type: 'error'
 	// 	});
 	// }
-
+	
 	// Alertas
 	async mostrarError(header: string, subHeader: string) {		
 		const alert = await this.alertController.create({
@@ -135,15 +135,15 @@ export class AlertService {
 		});
 		await alert.present();
 	}
-
+	
 	
 	/**
-	* Muestra un SweetAlert de tipo `success`.
-	* 
-	* @param title Título del SweetAlert
-	* @param text Texto del SweetAlert
-	* @return Promise<SweetAlertResult>
-	*/
+	 * Muestra un SweetAlert de tipo `success`.
+	 * 
+	 * @param title Título del SweetAlert
+	 * @param text Texto del SweetAlert
+	 * @return Promise<SweetAlertResult>
+	 */
 	async success(header: string, subHeader: string) {
 		const alert = await this.alertController.create({
 			header,
